75a536cccd230a799ee2d3c2cb9ceffd6ae6130b
Varun Iyer <varun_iyer@posteo.net>
2023-02-04 23:15:23 -0800

    add ucla loading

--- analysis.py
+++ analysis.py
@@ -1,12 +1,15 @@
 from dataclasses import dataclass, asdict, fields
+from typing import Union
 import csv
 from uuid import uuid4
 from enum import Enum, auto
+import re
 
 
 DATA_ROOT = "./"
 ASU_CONVOCATION_PATH = f"{DATA_ROOT}/asu/convocation_defaultveg_deidentified.csv"
 ASU_RESEARCH_DAY_PATH = f"{DATA_ROOT}/asu/researchday_defaultveg_deidentified.csv"
+UCLA_PATH = f"{DATA_ROOT}/ucla/combined_data.csv"
 
 
 class ResponseSource(str, Enum):
@@ -22,6 +25,21 @@ class Meal(str, Enum):
     MEAT = "meat"
     VEG = "veg"
 
+    @staticmethod
+    def other(meal: "Meal"):
+        if meal == Meal.MEAT:
+            return Meal.VEG
+        return Meal.MEAT
+
+    @staticmethod
+    def parse(text: str) -> Union["Meal", None]:
+        text = text.lower()
+        if "meat" in text:
+            return Meal.MEAT
+        if "plant" in text or "veg" in text:
+            return Meal.VEG
+        return None
+
 
 class Diet(str, Enum):
     VEGAN = "vegan"
@@ -29,6 +47,22 @@ class Diet(str, Enum):
     PESCATARIAN = "pescatarian"
     OMNIVORE = "omnivore"
     WRITTEN = "written"
+
+    @staticmethod
+    def parse(text: str) -> Union["Diet", None]:
+        text = text.lower()
+        diet_map = {
+            "other": Diet.WRITTEN,
+            "vegetarian": Diet.VEGETARIAN,
+            "vegan": Diet.VEGAN,
+            "none of the above": Diet.OMNIVORE,
+            "pescatarian": Diet.PESCATARIAN,
+            "na": Diet.OMNIVORE,
+            "none": Diet.OMNIVORE,
+        }
+        if text in diet_map:
+            return diet_map[text]
+        return None
 
 
 class AgeRange(str, Enum):
@@ -39,11 +73,27 @@ class AgeRange(str, Enum):
     AGE_55_64 = "55-64"
     AGE_65_74 = "65-74"
 
+    @staticmethod
+    def parse(text: str) -> Union["AgeRange", None]:
+        ages = re.search("\d\d-\d\d", text)
+        try:
+            return AgeRange(ages.group())
+        except (ValueError, AttributeError):
+            return None
+
 
 class Gender(str, Enum):
     MALE = "male"
     FEMALE = "female"
     NONBINARY = "nonbinary"
+
+    @staticmethod
+    def parse(text: str) -> Union["Gender", None]:
+        try:
+            return Gender(text.lower())
+        except ValueError:
+            pass
+        return None
 
 
 class Race(str, Enum):
@@ -54,16 +104,49 @@ class Race(str, Enum):
     MULTIRACIAL = "multiracial"
     WRITTEN = "written"
 
+    @staticmethod
+    def parse(text: str) -> Union["Race", None]:
+        try:
+            return Race(text.lower())
+        except ValueError:
+            pass
+        try:
+            return ({
+                "black of african American": Race.BLACK_AA,
+                "other": Race.WRITTEN,
+                "mixed (mestiza)": Race.MULTIRACIAL,
+            })[text.lower()]
+        except KeyError:
+            pass
+        return None
+
 
 class Ethnicity(str, Enum):
     LATIN = "hispanic or latinx"
     NON_LATIN = "not hispanic or latinx"
+
+    @staticmethod
+    def parse(text: str) -> Union["Ethnicity", None]:
+        text = text.lower()
+        if "non" in text or "not" in text:
+            return Ethnicity.NON_LATIN
+        if "latin" in text:
+            return Ethnicity.LATIN
+        return None
 
 
 class Role(str, Enum):
     STAFF = "staff"
     GRAD = "graduate_student"
     UG = "undergraduate_student"
+
+    @staticmethod
+    def parse(text: str):
+        text = text.lower()
+        for role in Role:
+            if role.value.replace("_", " ") == text:
+                return role
+        return None
 
 
 class Likert(int, Enum):
@@ -73,11 +156,31 @@ class Likert(int, Enum):
     AGREE = 4
     STRONGLY_AGREE = 5
 
+    @staticmethod
+    def parse(text: str) -> Union['Likert', None]:
+        satisfaction_map = {
+            "very unsatisfied": Likert.STRONGLY_DISAGREE,
+            "unsatisfied": Likert.DISAGREE,
+            "neither satisfied nor unsatisfied": Likert.NEUTRAL,
+            "satisfied": Likert.AGREE,
+            "very satisfied": Likert.STRONGLY_AGREE,
+            "": None,
+            "not important": Likert.STRONGLY_DISAGREE,
+            "not too important": Likert.DISAGREE,
+            # "Not too important": Likert.NEUTRAL,
+            "important": Likert.AGREE,
+            "very important": Likert.STRONGLY_AGREE,
+        }
+        try:
+            return satisfaction_map[text.lower()]
+        except KeyError:
+            return None
+
 
 @dataclass
 class SurveyResponse:
     uuid: str
-    source_id: int
+    source_id: int | None
     source: ResponseSource
     default_meal: Meal
     selected_meal: Meal
@@ -123,7 +226,7 @@ class SurveyResponse:
         )
 
     @staticmethod
-    def init_asu(asu_dict, source: ResponseSource) -> "SurveyResponse":
+    def init_asu(asu_dict: dict, source: ResponseSource) -> "SurveyResponse":
         diet_map = {
             "Other": Diet.WRITTEN,
             "Vegetarian": Diet.VEGETARIAN,
@@ -189,6 +292,30 @@ class SurveyResponse:
         )
 
     @staticmethod
-    def load_asu(path_to_csv: str, source: ResponseSource) -> list["SurveyResponse"]:
+    def init_ucla(ucla_dict: dict, source="") -> "SurveyResponse":
+        selected = Meal.parse(ucla_dict["mealSelection"])
+        return SurveyResponse(
+            uuid=uuid4(),
+            source_id=None,
+            source=ResponseSource.UCLA,
+            default_meal=selected if "Stay" in ucla_dict["mealSelection"] else Meal.other(selected),
+            selected_meal=selected,
+            eaten_meal=Meal.parse(ucla_dict["mealEaten"]),
+            diet=Diet.parse(ucla_dict["dietaryInfo"]),
+            diet_text=ucla_dict["dietaryInfo"],
+            race=Race.parse(ucla_dict["race"]),
+            race_text=ucla_dict["race"],
+            ethnicity=Ethnicity.parse(ucla_dict["ethnicity"]),
+            gender=None,
+            age=AgeRange.parse(ucla_dict["age"]),
+            role=Role.parse(ucla_dict["employment"]),
+            is_satisfied=Likert.parse(ucla_dict["satisfaction"]),
+            veg_is_important=None,
+            importance_reason=ucla_dict["rationale"],
+            other_fields={"importance": ucla_dict["importance"]},
+        )
+
+    @staticmethod
+    def load(function, path_to_csv: str, source: ResponseSource) -> list["SurveyResponse"]:
         reader = csv.DictReader(open(path_to_csv))
-        return [SurveyResponse.init_asu(row, source) for row in reader]
+        return [function(row, source) for row in reader]
